971a997a4646b60b20a67a117a7dc6db1c383a7b
Michael Rivera <soymrmike@gmail.com>
2018-09-28 01:41:54 -0700

    cleaning up some unneeded code.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,5 @@
-import Vue from 'vue/dist/vue.esm.js';
+import Vue from 'vue/dist/vue.esm';
 import rootTable from '../purchase_orders.vue';
-
 
 window.invoke_render = function() {
   var vm = new Vue({
