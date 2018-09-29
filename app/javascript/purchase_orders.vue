53a1919f7819a42c38d3339b3f56658285138cc3
Michael Rivera <soymrmike@gmail.com>
2018-09-28 19:45:38 -0700

    can conditionally render Next button. uses GET query when doing so.

--- app/javascript/purchase_orders.vue
+++ app/javascript/purchase_orders.vue
@@ -14,13 +14,26 @@
     <strong>Page</strong>: {{ query_result.info.page }}
     of
     {{ Math.ceil(query_result.info.total / query_result.info.per_page) }}
-    <a :href="nextPage()">Next ></a>
+
+    <a :href="nextPageHref()" v-if="hasNextLink()">Next ></a>
   </div>
 </template>
 
 
 <script>
 import poRow from 'po_row.vue'
+import { serializeObj, getUrlQueryAsObj } from 'support/urlHelper'
+
+function nextLink(component) {
+    const maxPages = Math.ceil(component.query_result.info.total / component.query_result.info.per_page)
+    const nextPage = (component.query_result.info.page + 1)
+    if(nextPage > maxPages) return null;
+    
+    const query = getUrlQueryAsObj();
+    query.page = nextPage;
+
+    return serializeObj(query);
+}
 
 export default {
     props: ['query_result'],
@@ -28,8 +41,11 @@ export default {
         poRow
     },
     methods: {
-        nextPage: function() {
-            return "?page=" + (this.query_result.info.page + 1)
+        hasNextLink: function() {
+            return nextLink(this) !== null
+        },
+        nextPageHref: function() {
+            return "?" + nextLink(this);
         }
     }
 }
