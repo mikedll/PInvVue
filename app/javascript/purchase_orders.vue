b46a61229b92cd19716e0e09185f925bfbd2f645
Michael Rivera <soymrmike@gmail.com>
2018-09-28 22:59:37 -0700

    paginator in place.

--- app/javascript/purchase_orders.vue
+++ app/javascript/purchase_orders.vue
@@ -11,42 +11,19 @@
         <po-row v-for="i in query_result.results" v-bind:po="i" v-bind:key="i.id"></po-row>
       </tbody>
     </table>
-    <strong>Page</strong>: {{ query_result.info.page }}
-    of
-    {{ Math.ceil(query_result.info.total / query_result.info.per_page) }}
-
-    <a :href="nextPageHref()" v-if="hasNextLink()">Next ></a>
+    <paginator v-bind:query_info="query_result.info"></paginator>
   </div>
 </template>
 
 
 <script>
 import poRow from 'po_row.vue'
-import { serializeObj, getUrlQueryAsObj } from 'support/urlHelper'
-
-function nextLink(component) {
-    const maxPages = Math.ceil(component.query_result.info.total / component.query_result.info.per_page)
-    const nextPage = (component.query_result.info.page + 1)
-    if(nextPage > maxPages) return null;
-    
-    const query = getUrlQueryAsObj();
-    query.page = nextPage;
-
-    return serializeObj(query);
-}
+import paginator from 'paginator.vue'
 
 export default {
     props: ['query_result'],
     components: {
-        poRow
-    },
-    methods: {
-        hasNextLink: function() {
-            return nextLink(this) !== null
-        },
-        nextPageHref: function() {
-            return "?" + nextLink(this);
-        }
+        poRow, paginator
     }
 }
 </script>
