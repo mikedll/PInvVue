993d08347f0c3257d9c53d22b95675a41954b8de
Michael Rivera <soymrmike@gmail.com>
2019-02-23 22:04:20 -0800

    App Home root component, for holding a nav menu.

---
+++ app/javascript/appHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-fluid">
+    <purchase-orders></purchase-orders>
+  </div>
+</template>
+
+<script>
+import purchaseOrders from './purchaseOrders.vue'
+export default {
+  components: [purchaseOrders],
+}
+</script>
+
+<style scoped>
+</style>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,12 @@
 import Vue from 'vue/dist/vue.esm'
-import rootTable from '../purchase_orders.vue'
+import appHome from '../purchase_orders.vue'
 import purchaseOrder from '../purchase_order.vue'
 
 window.invoke_render = function() {
   var vm = new Vue({
     el: '#main-container',
     components: {
-      rootTable, purchaseOrder
+      appHome, purchaseOrder
     },
     data: function() {
       return {
